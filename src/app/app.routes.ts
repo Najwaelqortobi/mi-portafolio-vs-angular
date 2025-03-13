4325d1b9b4ffb2812410504bdf27d6a8f57b1821
najwaelqortobi <najwaelqortobi@gmail.com>
2025-03-13 23:10:20 +0100

    ruta por proyectos agregada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from '../components/inicio/inicio.component';
 import { SobreMiComponent } from '../components/sobre-mi/sobre-mi.component';
+import { ProyectosComponent } from '../components/proyectos/proyectos.component';
 
 
 export const routes: Routes = [
     { path: 'inicio', component: InicioComponent },
     {path: '', redirectTo: 'inicio',  pathMatch: 'full'},
-    {path : 'sobre-mi', component: SobreMiComponent}
+    {path : 'sobre-mi', component: SobreMiComponent},
+    {path :'proyectos', component: ProyectosComponent}
     
 ];
