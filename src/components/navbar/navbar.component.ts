5614e099a4bff844569f8b4dae9797beb1b564f5
najwaelqortobi <najwaelqortobi@gmail.com>
2025-03-14 18:55:21 +0100

    background body agregados y rutas del nav y footer funcionando

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink,} from '@angular/router';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
